417b85baf5ad2da83e4d0bfd450356b1cfc987fc
David Felce <davefelce@gmail.com>
2018-01-31 20:31:45 +0000

    Docker configs

---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Prepare log files and start outputting logs to stdout
+# mkdir -p /usr/local/var/log/uwsgi
+
+touch portfolio.log
+chmod 644 portfolio.log
+chown www-data portfolio.log
+
+chown www-data media
+
+# Webapp commands
+python3 manage.py makemigrations
+python3 manage.py migrate                  # Apply database migrations
+python3 manage.py collectstatic --noinput  # Collect static files
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
