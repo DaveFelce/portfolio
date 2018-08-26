8682121ee21d991dc07c627f262f480b355f604f
David Felce <davefelce@gmail.com>
2018-08-26 16:38:46 +0100

    SECRET_KEY in env

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3vycywvse9na-#h%@d4l(h+ygh7*&effhr+b!k7_(c838i9m7u'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
